8413f0ac8d5f37c7ba52162a79cca96b27227c7b
Alex Thomson <LXGaming@users.noreply.github.com>
2019-10-07 15:25:37 +1300

    Setup Twitch Webhook auto renewal

--- LXGaming.DiscordStream/Integration/Twitch/TwitchIntegration.cs
+++ LXGaming.DiscordStream/Integration/Twitch/TwitchIntegration.cs
@@ -12,30 +12,43 @@ namespace LXGaming.DiscordStream.Integration.Twitch {
         public static TwitchAPI TwitchApi { get; private set; }
 
         public static void Prepare() {
-            var config = DiscordStream.Instance.Config;
-            var twitchIntegrationCategory = config.IntegrationCategory.TwitchIntegrationCategory;
+            var twitchIntegrationCategory = DiscordStream.Instance.Config.IntegrationCategory.TwitchIntegrationCategory;
             CreateTwitchApi(twitchIntegrationCategory.Id, twitchIntegrationCategory.Token);
 
             Task.Run(() => {
-                var users = TwitchApi.Helix.Users.GetUsersAsync(null, new List<string>(twitchIntegrationCategory.Channels)).Result;
-                if (users == null) {
-                    DiscordStream.Instance.Logger.Warn("Failed to get users");
-                    return;
-                }
-
-                foreach (var user in users.Users) {
-                    DataManager.TwitchUserCache.Add(Convert.ToInt64(user.Id), user);
-
-                    DiscordStream.Instance.Logger.Info("Subscribing to {} ({})", user.Login, user.Id);
-                    TwitchApi.Helix.Webhooks.StreamUpDownAsync(
-                        config.WebCategory.StreamCallback + "?user_id=" + user.Id,
-                        WebhookCallMode.Subscribe,
-                        user.Id,
-                        // TODO Setup auto renewal
-                        TimeSpan.FromHours(48),
-                        WebManager.Secret.ToString());
+                var duration = TimeSpan.FromHours(72);
+                while (true) {
+                    Subscribe(duration);
+                    if (DiscordStream.Instance.State.WaitOne(duration, false)) {
+                        break;
+                    }
                 }
             });
+        }
+
+        private static void Subscribe(TimeSpan duration) {
+            var config = DiscordStream.Instance.Config;
+            if (config == null) {
+                return;
+            }
+
+            var users = TwitchApi.Helix.Users.GetUsersAsync(null, new List<string>(config.IntegrationCategory.TwitchIntegrationCategory.Channels)).Result;
+            if (users == null) {
+                DiscordStream.Instance.Logger.Warn("Failed to get users");
+                return;
+            }
+
+            foreach (var user in users.Users) {
+                DataManager.TwitchUserCache.Add(Convert.ToInt64(user.Id), user);
+
+                DiscordStream.Instance.Logger.Info("Subscribing to {} ({})", user.Login, user.Id);
+                TwitchApi.Helix.Webhooks.StreamUpDownAsync(
+                    config.WebCategory.StreamCallback + "?user_id=" + user.Id,
+                    WebhookCallMode.Subscribe,
+                    user.Id,
+                    duration,
+                    WebManager.Secret.ToString());
+            }
         }
 
         private static void CreateTwitchApi(string id, string token) {
