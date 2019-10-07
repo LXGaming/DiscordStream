33fc0881178fb4cb08ce82b040b58b31b2014f74
Alex Thomson <LXGaming@users.noreply.github.com>
2019-10-07 14:11:26 +1300

    Fix Webhook subscription

--- LXGaming.DiscordStream/Integration/Twitch/TwitchIntegration.cs
+++ LXGaming.DiscordStream/Integration/Twitch/TwitchIntegration.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using LXGaming.DiscordStream.Manager;
 using TwitchLib.Api;
 using TwitchLib.Api.Core.Enums;
@@ -14,15 +16,26 @@ namespace LXGaming.DiscordStream.Integration.Twitch {
             var twitchIntegrationCategory = config.IntegrationCategory.TwitchIntegrationCategory;
             CreateTwitchApi(twitchIntegrationCategory.Id, twitchIntegrationCategory.Token);
 
-            foreach (var channel in twitchIntegrationCategory.Channels) {
-                DiscordStream.Instance.Logger.Info("Subscribing to {}", channel);
-                TwitchApi.Helix.Webhooks.StreamUpDownAsync(
-                    config.WebCategory.StreamCallback + "?user_id=" + channel,
-                    WebhookCallMode.Subscribe,
-                    channel,
-                    TimeSpan.FromHours(1),
-                    WebManager.Secret.ToString());
-            }
+            Task.Run(() => {
+                var users = TwitchApi.Helix.Users.GetUsersAsync(null, new List<string>(twitchIntegrationCategory.Channels)).Result;
+                if (users == null) {
+                    DiscordStream.Instance.Logger.Warn("Failed to get users");
+                    return;
+                }
+
+                foreach (var user in users.Users) {
+                    DataManager.TwitchUserCache.Add(Convert.ToInt64(user.Id), user);
+
+                    DiscordStream.Instance.Logger.Info("Subscribing to {} ({})", user.Login, user.Id);
+                    TwitchApi.Helix.Webhooks.StreamUpDownAsync(
+                        config.WebCategory.StreamCallback + "?user_id=" + user.Id,
+                        WebhookCallMode.Subscribe,
+                        user.Id,
+                        // TODO Setup auto renewal
+                        TimeSpan.FromHours(48),
+                        WebManager.Secret.ToString());
+                }
+            });
         }
 
         private static void CreateTwitchApi(string id, string token) {
